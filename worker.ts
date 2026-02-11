c9edadad9449f304f99f8b44f147c6cffd72238a
MBanucu <michael.banucu@googlemail.com>
2026-02-11 01:21:52 +0100

    build: update portable-pty to 0.9.0
    - Update dependency version in Cargo.toml
    - Regenerate Cargo.lock with updated dependencies
    - Simplify worker.ts by removing Windows-specific handling in read loop

--- worker.ts
+++ worker.ts
@@ -29,14 +29,9 @@ self.onmessage = (event: Bun.BunMessageEvent<ReaderHandle>) => {
 			// Send data back to main thread
 			self.postMessage(data);
 		} else {
-			if (process.platform !== "win32") {
-				// No more data, break the loop
-				self.postMessage("");
-				break;
-			} else {
-				// unknown behavior on Windows, let's find out
-				Bun.sleepSync(100);
-			}
+			// No more data, break the loop
+			self.postMessage("");
+			break;
 		}
 	}
 };
